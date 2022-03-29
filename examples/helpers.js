b4700ef3dc029285ba57c92850e26afece929a6d
nhatduynguyen <nhatduy2211@gmail.com>
2022-03-29 17:15:27 +0700

    Update: examples

--- examples/helpers.js
+++ examples/helpers.js
@@ -2,7 +2,7 @@ var helpers = function () {};
 
 helpers.register = function (Handlebars) {
     Handlebars.registerHelper('convertRiskCode', function(value) {
-        return value !== "0" ? "Failed" : "Not Impacted";
+        return value !== "0" ? "Failed" : "Inconclusive";
     });
 };
 
