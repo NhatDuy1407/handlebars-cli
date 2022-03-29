8b9b1e02b6011dfc4c592012c120a3f880d6be43
nhatduynguyen <nhatduy2211@gmail.com>
2022-03-29 14:30:54 +0700

    New: initial code

---
+++ README.md
@@ -0,0 +1,39 @@
+# handlebars-cli
+
+Invoke handlebars from the commandline.
+Specifically, using to convert [ZAP](https://www.zaproxy.org/) reports to xml and show into Azure Devops Test Results
+
+## Installation
+    
+```sh
+$ npm install -g handlebars-cli
+```
+
+## Usage
+```sh
+Usage:
+    hbs data.json helpers.js < template.hbs > output.xml
+    data.json               Parse data
+    helpers.js              Custom helpers
+    template.hbs            Your template
+    output.xml              Output rendered templates
+```
+
+## Include helpers
+
+```js
+// helpers.js
+var helpers = function () {};
+
+helpers.register = function (Handlebars) {
+    Handlebars.registerHelper('convertRiskCode', function(value) {
+        return value !== "0" ? "Failed" : "Not Impacted";
+    });
+};
+
+module.exports = helpers;
+```
+
+# license
+
+MIT
---
+++ index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+var fs   = require('fs'),
+    args = require('optimist').argv,
+    hbs  = require('handlebars');
+var helpersFile = null;
+
+if (args._.length) {
+    try {
+        helpersFile = args._[1];
+        args = JSON.parse(fs.readFileSync(args._[0]).toString());
+
+        if (helpersFile) {
+            addHandlebarsHelpers(helpersFile);
+        }
+    } catch (e) { }
+}
+else for (var key in args) {
+    try {
+        args[key] = JSON.parse(args[key]);
+    } catch (e) {
+    }
+}
+
+function addHandlebarsHelpers(file) {
+    const handlebarsHelper = require("./" + file);
+   
+    if (handlebarsHelper && typeof handlebarsHelper.register === 'function') {
+        handlebarsHelper.register(hbs);
+    } else {
+        console.error(`WARNING: ${file} does not export a 'register' function, cannot import`);
+    }
+}
+
+function readStream(s, done) {
+    var bufs = [];
+    s.on('data', function(d) {
+        bufs.push(d);
+    });
+    s.on('end', function() {
+        done(null, Buffer.concat(bufs));
+    });
+    s.resume();
+}
+
+readStream(process.stdin, function(err, tmpl) {
+    function handle(tmpl, args) {
+        hbs.registerHelper('include', function (file, context, opt) {
+            var context = null == context ? args : context;
+            var f = fs.readFileSync(file);
+            return handle(f, context); 
+        });
+        var template = hbs.compile(tmpl.toString());
+        var result = template(args);
+        return result;
+    }
+    process.stdout.write(handle(tmpl, args));
+});
+
